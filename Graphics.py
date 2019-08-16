1f975bdecbf2c07a5e90287c6686864a8239c43f
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-08-16 11:28:29 -0500

    uploading Graphics.py .A

--- Graphics.py
+++ Graphics.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def graphic (orbitas, condiciones):
-	x = np.array(range(7)).reshape(-1,1)
+	x = np.array(range(9)).reshape(-1,1)
 	
 	for a, c in zip(orbitas,condiciones):
 		plt.plot(x, a, label="x = {}".format(c))
