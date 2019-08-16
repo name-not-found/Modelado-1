73f8d57393aa9dd05b2d23db39ab3858bf936906
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-08-16 11:18:26 -0500

    loading formulae.py .A

--- main.py
+++ main.py
@@ -4,16 +4,26 @@
 # Simulando un sistema dinámico
 from Graphics import graphic
 import numpy as np
+from sympy import *
+from readcol import fgetcols
 
-def f(x):
-    return x**2 + 1
+#def f(x):
+#    return x**2 + 1
+
+x = Symbol('x')
+formulae = fgetcols('formulae.dat')
+y = sympify(formulae[0])
+yprime = y.diff(x)
+f = lambdify(x,y,"numpy")
+
+
 
 def iterar(its):
     L = []
     for it in its:
         l = []
         l.append(it)
-        for _ in range(6):
+        for _ in range(8):
             l.append( f(l[-1]) )
         L.append( np.array(l).reshape(-1,1) )
     return L
