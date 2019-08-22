77fe2ccf81b838877d403c43aac0a7eaccbd3e39
Pablo <pablocm1998@gmail.com>
2019-08-22 16:17:55 -0500

    main.py update -P

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from sympy import *
 import sys
 from iterar import iterar
 
-
 if __name__ == "__main__":
 	x = Symbol('x')
 	y = sympify( initial_conditions.flags() ) #no evaluable
 	f = lambdify(x,y,'numpy') #funcion evaluable
 
 	_init = [0.1, 1., 10.]
-	arrays = iterar(_init)
+	arrays = iterar(_init,f)
 
 	Graphics.plotting(arrays, _init)
