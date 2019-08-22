8767f9655a9e143332ba963d276c2b9c913731ea
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-08-22 15:05:23 -0500

    uploading main.py .A

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
-#! /usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-# Simulando un sistema dinámico
+import initial_conditions
 import Graphics
 import numpy as np
 from sympy import *
 import sys
-
-file_flag = False
-equation_flag = False
-formulae_str = 'x'
-
-#./dynamic -f archivo.txt
-#./dynamic -e ecuacion
-n = len(sys.argv)
-for i in range(n):
-		if "-f" in sys.argv[i]:
-			file_flag = True
-			filename = sys.argv[i+1]
-		if '-e' in sys.argv[i]:
-			equation_flag = True
-			formulae_str = sys.argv[i+1]
-
-if file_flag:
-	with open("filename") as f:
-		formulae = f.readlines()
-		formulae_str = formulae[0]
-		print("Using archive mode with" + formulae_str)
-if equation_flag:
-	print("Using equation mode with" + formulae_str)
-
-x = Symbol('x')
-y = sympify(formulae_str) #no evaluable
-#yprime = y.diff(x)
-f = lambdify(x,y,'numpy') #funcion evaluable
 
 def iterar(its):
     L = []
@@ -45,7 +14,12 @@ def iterar(its):
         L.append( np.array(l).reshape(-1,1) )
     return L
 
-_init = [0.1, 1., 10.]
-arrays = iterar(_init)
+if __name__ == "__main__":
+	x = Symbol('x')
+	y = sympify( initial_conditions.flags() ) #no evaluable
+	f = lambdify(x,y,'numpy') #funcion evaluable
 
-Graphics.plotting(arrays, _init)
+	_init = [0.1, 1., 10.]
+	arrays = iterar(_init)
+
+	Graphics.plotting(arrays, _init)
