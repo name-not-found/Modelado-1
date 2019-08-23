0a2ccb96a0ea9232ca4da681770eefdc98763d16
Pablo <pablocm1998@gmail.com>
2019-08-23 12:00:25 -0500

    testing.sh file updated -P

--- testing.sh
+++ testing.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python3 main.py -e x**2+1 
+python3 main.py -e a*x^n+b -a 1. -b 1. -n 2 -x0 .1 -N 9
+
+#Tarea para el jueves
+#./dynamic.py -e a*x^n+b -a[a_ini,a_fin,a_step] -b [] -n [] -x0 [] -i [iter] 
+#quiere que en cada flag le pasesmos una lista ej. -a [1,10,1] -b [1,1,1]
+#regresa la grafica con todas las ecuaciiones (familia de ec)
