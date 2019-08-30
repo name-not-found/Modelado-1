b531b3e1c253b10e1e77aa42b77d757a6ea27870
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-08-30 10:47:32 -0500

    uploading final_version .A

--- Graphics.py
+++ Graphics.py
@@ -1,11 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plotting(orbita, N, nombre):
-	x = np.array(range(N)).reshape(-1,1)
+#def plotting(orbita, N, nombre):
+def plotting(caption, fig, ax, y1):
+	ax.plot(y1, label=caption)
+	#ax.set(xlabel="i", ylabel="$x_i$", title=titulo)
+	ax.grid()
 	
+	plt.yscale('log')
+	
+	"""
+	x = np.array(range(N)).reshape(-1,1)
 	plt.plot( np.array(range(N)), orbita)
 	
 	plt.yscale('log')
 	plt.savefig(nombre)
+	"""
 	#plt.show()
