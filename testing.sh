aeedb581849de6d44bdaa20fa66949124ec495b9
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-08-30 11:34:09 -0500

    .A

--- testing.sh
+++ testing.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-python3 main.py -f formulae.dat -a " 2. 3. 1." -b "0. 1. 1." -n "1. 2. 1." -x "1. 11. 1." -N 10
-#Tarea para el jueves
-#./dynamic.py -e a*x^n+b -a[a_ini,a_fin,a_step] -b [] -n [] -x0 [] -i [iter] 
-#quiere que en cada flag le pasesmos una lista ej. -a [1,10,1] -b [1,1,1]
-#regresa la grafica con todas las ecuaciiones (familia de ec)
+python3 main.py -f formulae.dat -a "2. 3. 1." -b "0. 1. 1." -n "1. 2. 1." -x "1. 11. 1." -N 9
