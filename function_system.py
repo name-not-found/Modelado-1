c92e75ff6d70ce6981d4bb646f130c6b4cf306a0
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-09-05 16:23:14 -0500

    uploading function_system .A

---
+++ function_system.py
@@ -0,0 +1,19 @@
+from sympy import *
+import numpy as np
+
+def function_system(eq0, a0, b0, x0, n0, noise_flag, noise):
+	x, a, b, n = Symbol('x'), Symbol('a'), Symbol('b'), Symbol('n')
+	y = sympify(eq0) #no evaluable
+	y = y.subs(a, a0)
+	y = y.subs(b, b0)
+	y = y.subs(n, n0)
+	
+	f = lambdify(x,y,'numpy') #funcion evaluable
+	
+	y1 = f(x0)
+	
+	if noise_flag:
+		for i,value in enumerate(y1):
+			y1[i] = value + random()*noise
+	
+	return y1
