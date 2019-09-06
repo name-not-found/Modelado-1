df78c7dee1957dfb58a364b8ff83eae5a508eb72
Pablo <pablocm1998@gmail.com>
2019-09-06 17:36:13 -0500

    testing.sh file updated -P

--- testing.sh
+++ testing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 main.py -f formulae.dat -a "2. 3. 1." -b "0. 1. 1." -n "1. 2. 1." -x "1. 11. 1." -N 9
+python3 main.py -f formulae.dat -a "4000 4001 1." -b "0 1. 1." -n "1. 2. 1." -x "0 1.75 0.1" -r 20 -histo 20 -o sn_data.txt
