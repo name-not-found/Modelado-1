30c90117eb3350cda8794ba48773020640af5602
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-09-06 12:04:40 -0500

    uploading function_system.py .A

--- function_system.py
+++ function_system.py
@@ -1,5 +1,6 @@
 from sympy import *
 import numpy as np
+import random
 
 def function_system(eq0, a0, b0, x0, n0, noise_flag, noise):
 	x, a, b, n = Symbol('x'), Symbol('a'), Symbol('b'), Symbol('n')
@@ -14,6 +15,6 @@ def function_system(eq0, a0, b0, x0, n0, noise_flag, noise):
 	
 	if noise_flag:
 		for i,value in enumerate(y1):
-			y1[i] = value + random()*noise
+			y1[i] = value + (random.random()-0.5)*2.0*noise
 	
 	return y1
