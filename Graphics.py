9f7f26c64a46a09c9061d7201749b4a1b403d174
Angelica <angelica.nayeli@comunidad.enes.mx>
2019-09-06 12:03:19 -0500

    uploading Graphics.py .P

--- Graphics.py
+++ Graphics.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib import colors
 #def plotting(orbita, N, nombre):
 def plotting(caption, fig, ax, y1, N, titulo, time):
 	ax.plot(time, y1, label=caption)
@@ -17,3 +17,13 @@ def plotting(caption, fig, ax, y1, N, titulo, time):
 	plt.savefig(nombre)
 	"""
 	#plt.show()
+
+def plot_histogram(fig_h, ax_h,f,g,bind):
+	data_histo=[]
+	for i, (v1,v2) in enumerate(zip(f,g)):
+		data_histo.append(v1-v2)
+	ax_h.hist(data_histo,bins=bind)
+	#axs_histo.hist(g, bins=bind)
+	
+def plot_observations(fig, ax, obs_z, obs_d, obs_derr):
+	ax.errorbar(obs_z, obs_d,  yerr=obs_derr, fmt='o')
