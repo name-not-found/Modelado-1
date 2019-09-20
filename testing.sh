9b9325045f101aad0f1867740093348d8b813e25
Pablo <pablocm1998@gmail.com>
2019-09-20 11:27:04 -0500

    update testing.sh -P

--- testing.sh
+++ testing.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 main.py -f formulae.dat -a "4000 4001 1." -b "0 1. 1." -n "1. 2. 1." -x "0 1.75 0.1" -r 20 -histo 20 -o sn_data.txt
+python3 main.py -f formulae.dat -a "4000 4001 1." -b "0 1. 1." -n "1. 2. 1."  -histo 20 -o sn_data_clean.txt
+#sudo rm -rf /usr/local/Cellar/python/3.7.3
